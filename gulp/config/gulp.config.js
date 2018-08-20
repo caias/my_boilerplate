f7fd8021b16136a265580dd6703c0d038bfd7e2a
caias <caias@wemakeprice.com>
2018-08-20 15:25:08 +0900

    v1.1.0

--- gulp/config/gulp.config.js
+++ gulp/config/gulp.config.js
@@ -1,73 +1,73 @@
 const isProduction= require('./gulp.env');
 const config = {
 
-    html : {
-        src     : [
-                    'src/**/*.html',
-                    './index.html'
-                ],
-        dest    : 'dist/'
-    },
+	html : {
+		src	 : [
+			'src/**/*.html',
+			'./index.html'
+		],
+		dest	: 'dist/'
+	},
 
-    js : {
-        src     : 'src/js/*.js',
-        dest    : 'dist/js'
-    },
+	js : {
+		src	 : 'src/js/*.js',
+		dest	: 'dist/js'
+	},
 
-    scss : {
-        src     : 'src/scss/**/*.s+(a|c)ss',
-        dest    : 'dist/css'
-    },
+	scss : {
+		src	 : 'src/scss/**/*.s+(a|c)ss',
+		dest	: 'dist/css'
+	},
 
 	scssOptions : {
-    	outputStyle	: isProduction ? 'compressed' : 'expanded', // nested, expanded, compact, compressed
-    	indentType	: 'space',
-    	indentWidth	: 2, // maximum:10
-    	precision	: 6,
-    	linefeed	:'lf' // cr , crlf, lf , lfcr
-    },
+		outputStyle	: isProduction ? 'compressed' : 'expanded', // nested, expanded, compact, compressed
+		indentType	: 'space',
+		indentWidth	: 2, // maximum:10
+		precision	: 6,
+		linefeed	:'lf' // cr , crlf, lf , lfcr
+	},
 
-    image: {
-        src     : [
-                    'src/images/**/*.+(png|jpg|gif|svg)',
-                    '!src/images/sprite/**/*.png'
-                  ],
-        dest    : 'dist/images'
-    },
+	image: {
+		src	 : [
+			'src/images/**/*.+(png|jpg|gif|svg)',
+			'!src/images/sprite/**/*.png'
+  	],
+	dest	: 'dist/images'
+	},
 
-    sprite:{
-        src     : [
-                    'src/images/sprite/**/*.png',
-                    '!src/images/sprite/retina/*.png'
-                ],
-        dest    : 'dist/images/sprite'
-    },
+	sprite:{
+		src	 : [
+			'src/images/sprite/**/*.png',
+			'!src/images/sprite/retina/*.png'
+		],
+		dest	: 'dist/images/sprite'
+	},
 
-    retina:{
-        src     : 'src/images/sprite/retina/*.png',
-        dest    : 'dist/images/sprite'
-    },
+	retina:{
+		src	 : 'src/images/sprite/retina/*.png',
+		dest	: 'dist/images/sprite'
+	},
 
-    lint:{
-        scss : './gulp/config/scsslint.yml',
-        html : './gulp/config/.htmllintrc.json'
-    },
+	lint:{
+		scss : './gulp/config/scsslint.yml',
+		html : './gulp/config/.htmllintrc.json'
+	},
 
-    browsers : [
-        'last 3 versions',
-        'Android 2.3',
-        'Android >= 4',
-        'Chrome >= 20',
-        'Firefox >= 15',
-        'Explorer >= 8',
-        'iOS >= 6',
-        'Opera >= 12',
-        'Safari >= 6'
-    ],
+	browsers : [
+		'last 3 versions',
+		'Android 2.3',
+		'Android >= 4',
+		'Chrome >= 20',
+		'Firefox >= 15',
+		'Explorer >= 8',
+		'iOS >= 6',
+		'Opera >= 12',
+		'Safari >= 6'
+	],
 
 	purifyOpt : {
 		info:true,
-		minify:true,
+		minify:isProduction ? true : false,
 		rejected:true
 	}
 };
